ca3f7e686d014da97f84bea8187b74e67c05c6d0
kateobrien <k8obrien2811@gmail.com>
2021-09-30 13:24:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,4 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(string => string.split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' '))
